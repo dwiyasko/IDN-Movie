d55e67649ec67ddbd417b2693c451929283f0520
Dwiyasko Wicaksono <dwiyasko.wicaksono@tabsquare.com>
2022-11-09 07:11:17 +0700

    [Initial Commit] Setup Project

---
+++ app/src/main/java/com/example/idnmedia/movielist/presentation/MovieListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.idnmedia.movielist.presentation
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieListFragment : Fragment()
---
+++ app/src/main/java/com/example/idnmedia/utils/Constant.kt
@@ -0,0 +1,9 @@
+package com.example.idnmedia.utils
+
+object Constant {
+
+    const val imageUrlPrefix = "https://image.tmdb.org/t/p/w500/"
+
+    const val REQUIRE_AUTH_KEY = "Require-Authentication"
+    const val REQUIRE_AUTH = "$REQUIRE_AUTH_KEY: true"
+}
